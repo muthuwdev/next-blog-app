8072b6af09106849f2d2d492db00761a93552fa7
unknown <muthumini.devtrio@gmail.com>
2022-06-30 14:49:59 +0530

    added next sanity integrartions

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,34 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-between p-4  mx-auto max-w-7xl">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          {/* <img
+            className="w-44 cursor-pointer object-cover"
+            src="/blogImg.jpg"
+            alt=""
+          /> */}
+          TechBitz
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5 cursor-pointer">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className=" text-white bg-green-600 px-4 py-1 rounded-full ">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,31 @@ import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Head>
-        <title>DINU Blog</title>
+        <title>TechBitz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div className="flex items-center justify-between bg-blue-200  py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif ">
+            <span className="text-blue-900">TechBitz</span> with Latest Tech
+            News, Discover and Stay Connected!
+          </h1>
+          <h2>
+            Get access to the evolving tech news and publish your Tech updates
+            on the move!
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-30 lg:h-3/6"
+          src="introImgd.jpg"
+          alt=""
+        />
+      </div>
+      <div></div>
     </div>
   );
 };
